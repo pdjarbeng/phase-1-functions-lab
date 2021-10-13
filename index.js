d59a3b17255148756d1332ea6cb6dc15d36cef87
priscilla <pdjarbeng@outlook.com>
2021-10-13 09:36:31 -0400

    commit tree d1d6b7fcc09eb74c2223a1b8e64069934e7a25cd

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(block1, block2){
+    return 50 - 42
+    console.log(distanceFromHqInBlocks)
+}
+
+function distanceFromHqInFeet(feet1, feet2){
+    distanceFromHqInBlocks()
+}
